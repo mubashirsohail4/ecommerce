f953a216340af4def7e7089a52d35da28844bb03
Mubashir Sohail <mubashirsohail4444@gmail.com>
2025-04-09 23:38:07 +0500

    Install and add DaisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 function App() {
 
-  return (<div> <h1 className="text-8xl text-red-600">Hey</h1></div>);
+  return (<div>
+    <h1 className="text-8xl text-red-600">Hey</h1>
+    <button class="btn btn-primary">Button</button>
+    </div>);
 }
 
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -7,5 +9,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "pastel",
+      "retro",
+      "coffee",
+      "forest",
+      "cyberpunk",
+      "synthwave",
+      "luxury",
+      "autumn",
+      "valentine",
+      "aqua",
+      "business",
+      "night",
+      "dracula",
+    ],
+  },
 }
