8d5b76ba0ae6604e4cc895592e3806428d36ddb8
Mubashir Sohail <mubashirsohail4444@gmail.com>
2025-04-09 21:54:39 +0500

    Add cors and json middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors()); // CORS middleware to allow cross-origin requests
 app.use(morgan('dev')); // Logging middleware
 app.use(helmet()); // Security middleware
 
