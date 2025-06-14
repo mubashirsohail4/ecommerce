f245bd0e9dbf70edd22d6385ca69bd3ed46c92ec
Mubashir Sohail <mubashirsohail4444@gmail.com>
2025-06-14 21:08:59 +0500

    Add the rest of the functionalities matching source project

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,86 +2,10 @@ import { useEffect } from "react";
 import { useProductStore } from "../store/useProductStore";
 import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
 import ProductCard from "../components/ProductCard";
-// import AddProductModal from "../components/AddProductModal";
+import AddProductModal from "../components/AddProductModal";
 
 function HomePage() {
   const { products, loading, error, fetchProducts } = useProductStore();
-
-  // Testing
-  // let products = [
-  //   {
-  //     id: 1,
-  //     name: "Product 1",
-  //     price: 10.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Product 2",
-  //     price: 20.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Product 3",
-  //     price: 30.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Product 4",
-  //     price: 40.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Product 5",
-  //     price: 50.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "Product 6",
-  //     price: 60.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 7,
-  //     name: "Product 7",
-  //     price: 70.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 8,
-  //     name: "Product 8",
-  //     price: 80.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 9,
-  //     name: "Product 9",
-  //     price: 90.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 10,
-  //     name: "Product 10",
-  //     price: 100.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 11,
-  //     name: "Product 11",
-  //     price: 110.0,
-  //     image: "https://picsum.photos/500/600",
-  //   },
-  //   {
-  //     id: 12,
-  //     name: "Product 12",
-  //     price: 120.0,
-  //     image: "https://picsum.photos/500/600",
-  //   }
-  // ];
 
   useEffect(() => {
     fetchProducts();
@@ -89,7 +13,7 @@ function HomePage() {
 
   return (
     <main className="max-w-6xl mx-auto px-4 py-8 ">
-      {/* <div className="flex justify-between items-center mb-8">
+      <div className="flex justify-between items-center mb-8">
         <button
           className="btn btn-primary"
           onClick={() => document.getElementById("add_product_modal").showModal()}
@@ -100,13 +24,13 @@ function HomePage() {
         <button className="btn btn-ghost btn-circle" onClick={fetchProducts}>
           <RefreshCwIcon className="size-5" />
         </button>
-      </div> */}
+      </div>
 
-      {/* <AddProductModal /> */}
+      <AddProductModal />
 
-      {/* {error && <div className="alert alert-error mb-8">{error}</div>} */}
+      {error && <div className="alert alert-error mb-8">{error}</div>}
 
-      {/* {products.length === 0 && !loading && (
+      {products.length === 0 && !loading && (
         <div className="flex flex-col justify-center items-center h-96 space-y-4">
           <div className="bg-base-100 rounded-full p-6">
             <PackageIcon className="size-12" />
@@ -118,7 +42,7 @@ function HomePage() {
             </p>
           </div>
         </div>
-      )} */}
+      )}
 
       {loading ? (
         <div className="flex justify-center items-center h-64">
