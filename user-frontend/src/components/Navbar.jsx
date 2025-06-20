3827eb655ab962fe82112eb3bd82449c3c91fd64
Mubashir Sohail <mubashirsohail4444@gmail.com>
2025-06-20 22:34:22 +0500

    Upgrade tailwind and daisyui versions
    - upgrade tailwind from v3 to v4
    - upgrade daisyui from v4 to v5

--- user-frontend/src/components/Navbar.jsx
+++ user-frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto">
-        <div className="navbar px-4 min-h-[4rem] justify-between">
+        <div className="navbar px-4 min-h-16 justify-between">
           {/* LOGO */}
           <div className="flex-1 lg:flex-none">
             <Link to="/" className="hover:opacity-80 transition-opacity">
@@ -20,7 +20,7 @@ function Navbar() {
                 <ShoppingCartIcon className="size-9 text-primary" />
                 <span
                   className="font-semibold font-mono tracking-widest text-2xl 
-                    bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                    bg-clip-text text-transparent bg-linear-to-r from-primary to-secondary"
                 >
                   BARGAINSTORE
                 </span>
